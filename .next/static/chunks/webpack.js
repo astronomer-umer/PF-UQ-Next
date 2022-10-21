f693418de7ed65fa7b2151006ee87060b79c850e
Umer Qaiser <umer.qaiser@frizhub.com>
2022-10-21 16:31:24 +0500

    .

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0376114d7592c75b"; }
+/******/ 		__webpack_require__.h = function() { return "7c2677c67afb72e2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
